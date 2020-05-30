b3165ca6ff0355762113ea4eba6b7219d7097120
Flavio Alexandre <alexandre.flavio658@gmail.com>
2020-05-30 18:38:02 -0300

    Aumentando segurança da API

--- server.js
+++ server.js
@@ -2,32 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes');
-var cors = require("cors");
-var helmet = require("helmet");
 var mysql = require('mysql');
 var path = require('path');
-
-
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});
-app.use(helmet());
+var helmet = require('helmet');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //Conexao com o banco será feita no arquivo db.js
 const dbfun = require('./db');
 
-app.use(cors({
-  origin: ["https://comparie.com.br:3001"],
-  methods: ["GET", "POST", "PUT"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 //Permite Acesso externo na aplicação
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,x-access-token, Content-Type, Accept");
+  next();
+});
+app.use(helmet());
 //Pasta utilizada para o frontEnd vai ser a pasta WWW
 app.use('/', express.static(__dirname + '/public'));
 
